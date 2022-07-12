cea147adbe2706a5f6ec313f2274d1cd68aa8604
Felix-fale <falefelix23@gmail.com>
2022-07-12 15:52:44 +0000

    ajout du fichier php du news letter

---
+++ conf.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$dbase = "newsletter";
+$username = "root";
+$password = "root";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=newsletter", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+
+    //recuperation des valeurs saisie par le user
+    $nom = $_POST['name'];
+    $email = $_POST['email'];
+    $condition = $_POST['condition'];
+
+    if (isset($conn)) {
+        if (empty($nom && $email && $condition )) {
+            echo 'vous devez remplir tous les champs';
+        } else {
+            $sql = "INSERT INTO membres (names, email, nos_conditions)
+            VALUES ('$nom', '$email', '$condition')";
+            $conn->exec($sql);
+            echo "vous êtes connecté";
+            echo "<a href= 'connexion.html'>Aller à la page de connexion</a> ";
+        }
+    }
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
